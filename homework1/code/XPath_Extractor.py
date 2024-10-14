537bbe03ba0d255fd52ae083f518057b05d6dda2
AntonioSouls <tonyplanza1@gmail.com>
2024-10-14 12:21:39 +0200

    Avanzamento nella logica di XPath Estractor

--- homework1/code/XPath_Extractor.py
+++ homework1/code/XPath_Extractor.py
@@ -4,10 +4,14 @@ from lxml import html
 
 
 
-# def extract_information(HTML_file_path):
-#     with open(HTML_file_path, "r", encoding="utf-8") as file:
-#         tree = html.fromstring(file.read())
-#     return
+def extract_information(HTML_file_path):
+    elenco_tabelle = []
+    data = {}
+    with open(HTML_file_path, "r", encoding="utf-8") as file:
+        tree = html.fromstring(file.read())
+        elenco_tabelle = tree.xpath("//table[contains(@class,'ltx_tabular')]")
+
+    return elenco_tabelle
 
 
 # Funzione principale per costruire i JSON corrispondenti agli HTML della directory 'source'
@@ -19,7 +23,7 @@ def main():
         print(f"Processing: {HTML_file}\n")
         HTML_file_path = os.path.join(cartella, HTML_file)        # Recupera il path di ogni elemento esaminato
         information = extract_information(HTML_file_path)         # Invoca la funzione per estrarre le informazioni dall'elemento
-
+        break
         # Crea il file JSON corrispondente e ci salva dentro l'informazione estratta
         JSON_file_name = HTML_file.replace(".html", ".json")
         JSON_file_path = f"homework1/extraction/{JSON_file_name}"
