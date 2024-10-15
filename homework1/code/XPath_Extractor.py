45891c94173df519bf61cf87dabcc7d90480232b
danielluca28 <dani28settembre@gmail.com>
2024-10-15 11:09:51 +0200

    improved html files encoding

--- homework1/code/XPath_Extractor.py
+++ homework1/code/XPath_Extractor.py
@@ -3,7 +3,6 @@ import json
 from lxml import html
 from lxml import etree
 
-
 # Funzione per estrarre i dati in formato JSON da un documento HTML. I dati in formato JSON vengono poi mandati
 # alla funzione principale che si occupa di salvarli in un file opportuno
 def extract_information(HTML_file_path):
@@ -11,9 +10,10 @@ def extract_information(HTML_file_path):
     data = {}
 
     # Apro il file HTML ricevuto come parametro e ci estraggo l'HTML TREE
-    with open(HTML_file_path, "r", encoding="utf-8") as file:
-        tree = html.fromstring(file.read())
-        elenco_figure = tree.xpath("//figure[contains(@class,'ltx_table')]")       # Trovo tutti i tag <figure> nell'HTML
+    with open(HTML_file_path, "rb") as file:  # Open the file in binary mode
+        html_bytes = file.read()
+        tree = html.fromstring(html_bytes)
+        elenco_figure = tree.xpath("//figure[contains(@class,'ltx_table')]")  # Trovo tutti i tag <figure> nell'HTML
 
         # Per ogni tag <figure> estraggo le informazioni che mi servono per costruire il JSON
         for i, figure in enumerate(elenco_figure, start=1):
@@ -23,16 +23,14 @@ def extract_information(HTML_file_path):
             reference_paragraphs = tree.xpath(f"//a[contains(@title, 'Table {i}')]/ancestor::p")
             # footnotes = table.xpath("./following-sibling::footnote/text()")
 
-
             table_data = []
             for table in tables:
                 table_data.append(etree.tostring(table, pretty_print=True).decode())
-            
+
             references_data = []
             for reference in reference_paragraphs:
                 paragraph_text = reference.xpath("string()").strip()
                 references_data.append(paragraph_text)
-            
 
             # Strutturo le informazioni raccolte in un formato JSON compatibile, per poi passare tale formato alla funzione principale
             # che si occupa di salvarlo in un opportuno file JSON
@@ -44,8 +42,6 @@ def extract_information(HTML_file_path):
             }
         return data
 
-
-
 # Funzione principale per costruire i JSON corrispondenti agli HTML della directory 'source'
 def main():
     cartella = "homework1/sources"
@@ -53,17 +49,17 @@ def main():
     # Esamina ogni elemento della directory 'source'
     for HTML_file in os.listdir(cartella):
         print(f"Processing: {HTML_file}\n")
-        HTML_file_path = os.path.join(cartella, HTML_file)        # Recupera il path di ogni elemento esaminato
-        information = extract_information(HTML_file_path)         # Invoca la funzione per estrarre le informazioni dall'elemento
-        
+        HTML_file_path = os.path.join(cartella, HTML_file)  # Recupera il path di ogni elemento esaminato
+        information = extract_information(HTML_file_path)  # Invoca la funzione per estrarre le informazioni dall'elemento
+
         # Crea il file JSON corrispondente e ci salva dentro l'informazione estratta
         JSON_file_name = HTML_file.replace(".html", ".json")
         JSON_file_path = f"homework1/extraction/{JSON_file_name}"
         with open(JSON_file_path, 'w', encoding="utf-8") as output_file:
-            json.dump(information, output_file, ensure_ascii=False, indent=4)  # DA RIVEDERE
+            json.dump(information, output_file, ensure_ascii=False, indent=4)
         print(f"Created: {JSON_file_name}\n")
-        
-    print(f"SUCCESS: All JSONs are been created")
+
+    print(f"SUCCESS: All JSONs have been created")
 
 # Starter dello script
 if __name__ == "__main__":
